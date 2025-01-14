0f76cce4cfddb77507a9b1f301b9661fd6b9e4fb
Zachary Gazaille <zacharygazaille@gmail.com>
2025-01-13 22:44:45 -0500

    Add icons to navbar
    Adds icons to navbar and makes it so current page icon is highlighted in
    black

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Link, Navigate } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate, useLocation } from 'react-router-dom';
 import './Navbar.css';
+import { PiHouseFill } from "react-icons/pi";
+import { BiSolidPlusSquare } from "react-icons/bi";
+import { BiSolidExit } from "react-icons/bi";
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -19,6 +21,8 @@ const Navbar = () => {
         navigate('/create');
     }
 
+    const location = useLocation();
+
     return (
         <nav className="navbar">
             <div className="navbar-container">
@@ -27,13 +31,22 @@ const Navbar = () => {
                 </Link>
                 <ul className='nav-menu'>
                     <li className="nav-item">
-                        <button onClick={navigateHome} className='nav-button'>Home</button>
+                        <button onClick={navigateHome} className={`nav-button ${location.pathname === '/' ? 'active' : ''}`}>
+                            <PiHouseFill className="nav-icon" />
+                            Home
+                        </button>
                     </li>
                     <li className="nav-item">
-                        <button onClick={navigateCreate} className='nav-button'>New Event</button>
+                        <button onClick={navigateCreate} className={`nav-button ${location.pathname === '/create' ? 'active' : ''}`} >
+                            <BiSolidPlusSquare className="nav-icon" />
+                            New Event
+                        </button>
                     </li>
                     <li className="nav-item">
-                        <button onClick={handleLogout} className='nav-button'>Logout</button>
+                        <button onClick={handleLogout} className='nav-button'>
+                            <BiSolidExit className='nav-icon' />
+                            Logout
+                        </button>
                     </li>
                 </ul>
             </div>
