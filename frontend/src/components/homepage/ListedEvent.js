4f6c2635e38af11897b2ff8284ef9c9df5a2c04f
Zachary Gazaille <zacharygazaille@gmail.com>
2025-01-29 20:08:31 -0500

    Style the ListedEvent components
    Styles the listed event component and adds all the info it needs.

--- frontend/src/components/homepage/ListedEvent.js
+++ frontend/src/components/homepage/ListedEvent.js
@@ -2,10 +2,17 @@ import './ListedEvent.css';
 import React from 'react';
 
 export default function ListedEvent({ event, onClick, selected }) {
+    const formatDate = (dateString) => {
+      const options = { month: 'long', day: 'numeric', year: 'numeric' };
+      return new Date(dateString).toLocaleDateString('en-US', options);
+    };
+
     return (
       <div className={`listed-event ${selected ? 'selected-event' : ''}`} onClick={onClick}>
-          <p>{event.title}</p>
-          <p>{event.date} at {event.time}</p>
+          <p className='event-title listed-event-item'>{event.title}</p>
+          <div className='listed-event-item'><p className='event-item-key'>Location: </p><p className='event-location'>{event.location}</p></div>
+          <div className='listed-event-item'><p className='event-item-key'>Time: </p><p className='event-time'>{formatDate(event.date)} at {event.time}</p></div>
+          <div className='listed-event-item'><p className='event-item-key'>Event Planner: </p><p className='event-organizer'>{event.organizer}</p></div>
       </div>
     );
   }
